8328310ba526aa5289afdc7e0a6d369b44aaba2b
Neil Dobson <ndobson@barnardos.org.au>
2020-02-02 21:08:27 +1100

    Client test

---
+++ vue_spa.Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using ServiceStack;
+using System;
+
+namespace vue_spa.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var authClient = new JsonServiceClient("https://localhost:5001/");
+
+            var authResponse = authClient.Post(new Authenticate {
+                provider = "credentials",
+                UserName = "new@user.com",
+                Password = "p@55wOrd",
+                RememberMe = true,
+            });
+
+            var client = new JsonServiceClient("https://localhost:5001") {
+                BearerToken = authResponse.BearerToken //Send JWT in HTTP Authorization Request Header
+            };
+        }
+    }
+}
